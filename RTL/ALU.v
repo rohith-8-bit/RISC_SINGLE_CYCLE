84c486a6c91c5a35fe5d4e720959e73ff656dfd6
Rohith <pes1202203689@pesu.pes.edu>
2025-08-07 16:50:58 +0530

    Update ALU.v

--- RTL/ALU.v
+++ RTL/ALU.v
@@ -1,7 +1,8 @@
-module ALU(A,B,ALUControl,Result,Negative,Zero,Carry,Overflow);
+module ALU(A,B,ALUControl,Result,Negative,Zero,Carry,Overflow, rst);
 
 input [31:0]A,B;
 input [2:0]ALUControl;
+input rst;
 output [31:0]Result;
 output Negative,Zero,Carry,Overflow;
 
@@ -18,10 +19,10 @@ assign Result = (ALUControl[2:0] == 3'b000) ? sum :
                 (ALUControl[2:0] == 3'b011) ? (A | B) :
                 (ALUControl[2:0] == 3'b101) ? {{31{1'b0}},(sum[31])} : 32'd0 ;
                 
-assign Negative = Result[31]; //negative flag
-assign Zero     = &(~Result); //zero flag
-assign Carry    = (cout & (~ALUControl[1])); //carry flag
-assign Overflow = (~ALUControl[1]) & (sum[31] ^ A[31]) & (~(A[31 ^ B[31] ^ ALUControl[0]])); //overflow flag 
+assign Negative = (rst == 1'b0)? 1'b0 : Result[31]; //negative flag
+assign Zero     = (rst == 1'b0)? 1'b0 : &(~Result); //zero flag
+assign Carry    = (rst == 1'b0)? 1'b0 : (cout & (~ALUControl[1])); //carry flag
+assign Overflow = (rst == 1'b0)? 1'b0 : (~ALUControl[1]) & (sum[31] ^ A[31]) & (~(A[31 ^ B[31] ^ ALUControl[0]])); //overflow flag 
 
 
 endmodule
